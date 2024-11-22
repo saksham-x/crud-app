3705717df1415d5517ea4084692a440f3450ed71
saksham-x <sakshampokharel2002@gmail.com>
2024-11-22 19:02:55 +0545

    update name

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser, getUsers, getUserById, updateUser, deleteUser } = require("../controllers/userController");
+const { createUser, getUsers, getUserById, updateUser, deleteUser } = require("../controllers/user.controller");
 
 
 const router = express.Router();
