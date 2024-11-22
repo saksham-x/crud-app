182203972043d57c5713fea71bb9a3e02b38c990
saksham-x <sakshampokharel2002@gmail.com>
2024-11-22 23:32:54 +0545

    Completed

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const { createUser, getUsers, getUserById, updateUser, deleteUser } = require("../controllers/user.controller");
-const { validateUser } = require("../middlewares/validation.middleware");
+const { validationMiddleware } = require("../middlewares/index.middleware");
+const { userController } = require("../controllers/index.controller");
+// const { createUser, getUsers, getUserById, updateUser, deleteUser } = require("../controllers/user.controller");
+// const { validateUser } = require("../middlewares/validation.middleware");
+
 
 
 const router = express.Router();
 
 
-router.post('/',validateUser, createUser)
-router.get('/', getUsers)
-router.get('/:id', getUserById)
-router.put('/:id',validateUser, updateUser)
-router.delete('/:id', deleteUser)
+router.post('/', validationMiddleware.validateUser, userController.createUser)
+router.get('/', userController.getUsers)
+router.get('/:id', userController.getUserById)
+router.put('/:id', validationMiddleware.validateUser, userController.updateUser)
+router.delete('/:id', userController.deleteUser)
 
 
 module.exports = router;
